31756a6de1e08ce7ff5cb5eebe6cf9bcba665d2c
zhouzhi <zhouzhi073859@outlook.com>
2018-09-20 17:24:35 +0800

    :package: Updating compiled files or packages.

--- app.js
+++ app.js
@@ -10,6 +10,17 @@
  */
 const Tclass = require('./class');
 const functions = require('./function');
-const nowTime = Date.now();
-const result = functions.sum(100000);
-console.log('结果为：' + result + '，耗时' + ((Date.now()) - nowTime) + 'ms');
+const fibonacci = require('./fibonacci');
+const nowTime1 = Date.now();
+const result1 = functions.fib_a(40);
+console.log('compute in recursion with napi module: ' + result1 + ',expend' + ((Date.now()) - nowTime1) + 'ms');
+const nowTime2 = Date.now();
+const result2 = functions.fib_b(1000000);
+console.log('compute in loop with napi module: ' + result2 + ',expend' + ((Date.now()) - nowTime2) + 'ms');
+const nowTime3 = Date.now();
+const result3 = fibonacci.fibonacci_a(40);
+console.log('compute in recursion with js module: ' + result3 + ',expend' + ((Date.now()) - nowTime3) + 'ms');
+const nowTime4 = Date.now();
+const result4 = fibonacci.fibonacci_b(40);
+console.log('compute in loop with js module: ' + result4 + ',expend' + ((Date.now()) - nowTime4) + 'ms');
+
--- src/function.cc
+++ src/function.cc
@@ -1,7 +1,11 @@
 #include <node_api.h>
-#include <iostream>
+#include <vector>
 
-static napi_value Sum(napi_env env, napi_callback_info info) {
+long fibonacci_a(int n);
+long fibonacci_b(int &n);
+long fibonacci_c(int &n);
+
+static napi_value fib_a(napi_env env, napi_callback_info info) {
   size_t argc = 1;
   napi_value args[1];
   napi_get_cb_info(env, info, &argc, args, NULL, NULL);
@@ -16,25 +20,81 @@ static napi_value Sum(napi_env env, napi_callback_info info) {
     return  errChar;
   }
 
-  long long int value0;
-  napi_get_value_int64(env, args[0], &value0);
+  int value0;
+  napi_get_value_int32(env, args[0], &value0);
 
-  long long int fbnqRes = 0;
-  long i;
-  for (i = 0; i <= value0; i++) {
-    fbnqRes = fbnqRes + i;
-  }
+  long fbnqRes = 0;
+  fbnqRes = fibonacci_a(value0);
 
   napi_value result;
-  napi_create_int64(env, fbnqRes, &result);
+  napi_create_int32(env, fbnqRes, &result);
+
+  return result;
+}
+
+static napi_value fib_b(napi_env env, napi_callback_info info) {
+  size_t argc = 1;
+  napi_value args[1];
+  napi_get_cb_info(env, info, &argc, args, NULL, NULL);
+
+  napi_valuetype valuetype0;
+  napi_typeof(env, args[0], &valuetype0);
+
+  if (!(valuetype0 == napi_number)) {
+    napi_value errChar;
+    napi_throw_error(env, NULL, "assertion (Wrong argument type. Numbers expected.) failed");
+    napi_create_string_utf8(env, "Wrong argument type. Numbers expected.", sizeof("Wrong argument type. Numbers expected."), &errChar);
+    return  errChar;
+  }
+
+  int value0;
+  napi_get_value_int32(env, args[0], &value0);
+
+  long fbnqRes = 0;
+  fbnqRes = fibonacci_b(value0);
+
+  napi_value result;
+  napi_create_int32(env, fbnqRes, &result);
 
   return result;
 }
 
 static napi_value Init(napi_env env, napi_value exports) {
-  napi_property_descriptor desc = { "sum", 0, Sum, 0, 0, 0, napi_default, 0 };
-  napi_define_properties(env, exports, 1, &desc);
+  napi_property_descriptor desc[2] = {
+    { "fib_a", 0, fib_a, 0, 0, 0, napi_default, 0 },
+    { "fib_b", 0, fib_b, 0, 0, 0, napi_default, 0 },
+  };
+  napi_define_properties(env, exports, 2, desc);
   return exports;
+}
+
+
+long fibonacci_a(int n) {
+  if (n < 2) {
+    return n;
+  }
+  return fibonacci_a(n - 2) + fibonacci_a(n - 1);
+}
+
+long fibonacci_b(int &n) {
+  long feb_n_val = 0;
+  int i;
+  long a = 1;
+  long b = 1;
+  if (n < 3) {
+    return 1;
+  }
+  for(i = 3;i <= n;i++) {
+    feb_n_val = a + b;
+    b = a;
+    a = feb_n_val;
+  }
+  return feb_n_val;
+}
+
+long fibonacci_c(int &n) {
+  // 准备列矩阵
+
 }
 
 NAPI_MODULE(NODE_GYP_MODULE_NAME, Init)
