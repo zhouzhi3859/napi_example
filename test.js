a7b5d3c10b6ef12b5259c283edcc7fedc0bf1e96
zhouzhi <zhouzhi073859@outlook.com>
2018-10-25 10:37:44 +0800

    :ok_hand: Updating code due to code review changes.

--- test.js
+++ test.js
@@ -14,17 +14,23 @@ setTimeout(function() {
   console.log('timeout1');
 }, 10);
 
-new Promise(resolve => {
+new Promise((resolve) => {
   console.log('promise1');
   resolve();
-  setTimeout(() => console.log('timeout2'), 10);
-  setTimeout(() => console.log('timeout3'), 0);
-  setImmediate(() => console.log('immediate'));
+  setTimeout(() => {
+    console.log('timeout2');
+  }, 10);
+  setTimeout(() => {
+    console.log('timeout3');
+  }, 0);
+  setImmediate(() => {
+    console.log('immediate');
+  });
   process.nextTick(() => {
     console.log('then2');
   });
 }).then(function() {
-  console.log('then1')
+  console.log('then1');
 });
 
 console.log('script end');
